018f032f840e2cf641d68c61175b653ed7f50a68
Wonjang Son <sondaniel.88@gmail.com>
2021-08-27 12:43:21 +0200

    Updated pm2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,41 @@
 module.exports = {
   apps: [
     {
-      name: "api",
-      script: "dist/index.js",
-      instances: process.env.APP_INSTANCES || 2,
+      name: '<app-name>',
+      script: 'dist/src/index.js',
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      exec_mode: 'cluster',
+      instances: 2,
+      kill_timeout: 5000,
+      autorestart: true,
+      max_restarts: 10,
+      watch: true,
+      watch_options: {
+        followSymlinks: false,
+      },
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        port: 8080,
+      },
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DDTHH:mm:ss.sssZ',
     },
   ],
+  // https://pm2.keymetrics.io/docs/usage/deployment/
+  deploy: {
+    production: {
+      key: process.env.EC2_PEM_PATH,
+      host: process.env.EC2_HOST,
+      user: 'ubuntu',
+      ref: 'origin/main',
+      repo: 'git@github.com:<username>/<repo-name>.git',
+      path: '/home/ubuntu',
+      'post-deploy':
+        'yarn && yarn clean && yarn build && pm2 startOrRestart ecosystem.config.js --env production && pm2 set pm2:autodump true && pm2 save',
+    },
+  },
 };
