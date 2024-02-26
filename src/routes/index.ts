82dd484676ea15e336ebff2263b2c1bf0fef0fbf
Rodrigo Alvarez <alvarezrrj@gmail.com>
2024-02-26 10:00:37 -0300

    🧹 Clean up

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,13 @@ import playersRouter from "./players.router";
 import bankAccountsRouter from "./bank-accounts.router";
 import transactionsRouter from "./transactions.router";
 import agentRouter from "./agent.router";
-// import agentsRouter from "./agent.router";
 
 const mainRouter = Router();
 
 mainRouter.use("/", homeRouter);
 mainRouter.use("/players", playersRouter);
 mainRouter.use("/bank-account", bankAccountsRouter);
-mainRouter.use("/transactions", transactionsRouter); // cashiIn, cashOut, y lista de transacciones pendientes de deposito.
+mainRouter.use("/transactions", transactionsRouter);
 mainRouter.use("/agent", agentRouter);
 
 export default mainRouter;
