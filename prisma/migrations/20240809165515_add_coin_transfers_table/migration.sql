25d313eb9275783e02b76ebaf01c7973c6581dfc
Dasit <foxdev@Dasits-MacBook-Pro.local>
2024-08-13 14:10:40 -0300

    fix migration remove if

--- prisma/migrations/20240809165515_add_coin_transfers_table/migration.sql
+++ prisma/migrations/20240809165515_add_coin_transfers_table/migration.sql
@@ -1,17 +1,14 @@
 -- 1. Verificar si las columnas existen antes de crearlas
--- BONUS
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
                WHERE TABLE_NAME = 'BONUS' AND COLUMN_NAME = 'coin_transfer_id') THEN
   ALTER TABLE `BONUS` ADD COLUMN `coin_transfer_id` VARCHAR(191);
 END IF;
 
--- DEPOSITS
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
                WHERE TABLE_NAME = 'DEPOSITS' AND COLUMN_NAME = 'coin_transfer_id') THEN
   ALTER TABLE `DEPOSITS` ADD COLUMN `coin_transfer_id` VARCHAR(191);
 END IF;
 
--- PAYMENTS
 IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
                WHERE TABLE_NAME = 'PAYMENTS' AND COLUMN_NAME = 'coin_transfer_id') THEN
   ALTER TABLE `PAYMENTS` ADD COLUMN `coin_transfer_id` VARCHAR(191);
@@ -46,16 +43,17 @@ ALTER TABLE `BONUS` MODIFY COLUMN `coin_transfer_id` VARCHAR(191) NOT NULL;
 ALTER TABLE `DEPOSITS` MODIFY COLUMN `coin_transfer_id` VARCHAR(191) NOT NULL;
 ALTER TABLE `PAYMENTS` MODIFY COLUMN `coin_transfer_id` VARCHAR(191) NOT NULL;
 
--- 5. Crear índices únicos y claves foráneas si no existen
-CREATE UNIQUE INDEX IF NOT EXISTS `BONUS_coin_transfer_id_key` ON `BONUS`(`coin_transfer_id`);
-CREATE UNIQUE INDEX IF NOT EXISTS `DEPOSITS_coin_transfer_id_key` ON `DEPOSITS`(`coin_transfer_id`);
-CREATE UNIQUE INDEX IF NOT EXISTS `PAYMENTS_coin_transfer_id_key` ON `PAYMENTS`(`coin_transfer_id`);
+-- 5. Crear índices únicos y claves foráneas
+CREATE UNIQUE INDEX `BONUS_coin_transfer_id_key` ON `BONUS`(`coin_transfer_id`);
+CREATE UNIQUE INDEX `DEPOSITS_coin_transfer_id_key` ON `DEPOSITS`(`coin_transfer_id`);
+CREATE UNIQUE INDEX `PAYMENTS_coin_transfer_id_key` ON `PAYMENTS`(`coin_transfer_id`);
 
-ALTER TABLE `PAYMENTS` ADD CONSTRAINT IF NOT EXISTS `PAYMENTS_coin_transfer_id_fkey` 
+-- Añadir claves foráneas sin IF NOT EXISTS
+ALTER TABLE `PAYMENTS` ADD CONSTRAINT `PAYMENTS_coin_transfer_id_fkey` 
 FOREIGN KEY (`coin_transfer_id`) REFERENCES `COIN_TRANSFERS`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `DEPOSITS` ADD CONSTRAINT IF NOT EXISTS `DEPOSITS_coin_transfer_id_fkey` 
+ALTER TABLE `DEPOSITS` ADD CONSTRAINT `DEPOSITS_coin_transfer_id_fkey` 
 FOREIGN KEY (`coin_transfer_id`) REFERENCES `COIN_TRANSFERS`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE `BONUS` ADD CONSTRAINT IF NOT EXISTS `BONUS_coin_transfer_id_fkey` 
+ALTER TABLE `BONUS` ADD CONSTRAINT `BONUS_coin_transfer_id_fkey` 
 FOREIGN KEY (`coin_transfer_id`) REFERENCES `COIN_TRANSFERS`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
